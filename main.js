a1c9761b7f25949557c4aa9e1fe2d9d9a54183e8
Yisak Girmay <yisakbruck23@gmail.com>
2023-04-26 16:39:55 -0400

    complete lab

--- main.js
+++ main.js
@@ -3,7 +3,31 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorModal = document.querySelector("#modal");
+errorModal.classList.add("hidden");
 
+const hearts = document.querySelectorAll(".like-glyph");
+hearts.forEach(heart => {
+  heart.addEventListener("click", event => {
+    mimicServerCall()
+      .then(() => {
+        if (heart.innerText === "♡") {
+          heart.innerText = "♥";
+          heart.classList.add("activated-heart");
+        } else {
+          heart.innerText = "♡";
+          heart.classList.remove("activated-heart");
+        }
+      })
+      .catch(error => {
+        errorModal.classList.remove("hidden");
+        errorModal.innerText = error;
+        setTimeout(() => {
+          errorModal.classList.add("hidden");
+        }, 3000);
+      });
+  });
+});
 
 
 
